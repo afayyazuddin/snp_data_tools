e4d4ba94e32141b1e68af786579880f61f631f1f
Amir Fayyazuddin <12455725+afayyazuddin@users.noreply.github.com>
2019-11-12 20:43:57 -0800

    Initial commit

---
+++ get_first_snps_in_file.py
@@ -0,0 +1,30 @@
+'''
+script to find the first rsid to map to identify genome build
+'''
+import os
+import sys
+from snp_data_tools import SNP
+
+directory = sys.argv[1]
+snp_list = []
+
+for file in os.listdir(directory):
+    os.listdir(directory)
+    if not file.startswith("."):
+        with open(directory + "/" + file, 'r') as infile:
+            coords = iter(infile.readlines())
+            next(coords)
+            while True:
+                try:
+                    snp = next(coords)
+                    if not (snp.startswith("RSID") or snp.startswith("#") or snp.startswith("rsid") or not snp.strip()):
+                        snp_row = SNP.convert(snp)
+                        rsid = snp_row.rsid
+                        if "rs" in rsid:
+                            break
+                except StopIteration:
+                    break
+            if rsid not in snp_list:
+                snp_list.append(rsid)
+                print(snp_list)
+print(*snp_list, sep='\n')
